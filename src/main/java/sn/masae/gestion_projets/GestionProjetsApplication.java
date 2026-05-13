89c3e87998bf2f6fec81d21204e479dd2c4a6d5f
taphiscoe <cmoustaphadiop@gmail.com>
2026-05-13 12:53:46 +0000

    premier commit - structure de base

---
+++ src/main/java/sn/masae/gestion_projets/GestionProjetsApplication.java
@@ -0,0 +1,13 @@
+package sn.masae.gestion_projets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionProjetsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionProjetsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sn/masae/gestion_projets/GestionProjetsApplicationTests.java
@@ -0,0 +1,13 @@
+package sn.masae.gestion_projets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionProjetsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
